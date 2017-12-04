b373d7e15b5ef39ab46758176bac229f0be986e0
dgafz <csantacruz112@gmail.com>
2017-12-04 01:50:02 -0500

    proyecto en django

--- ncancha/static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-  // selecion del toogle dle menu
-	var toggleMenu = $('#toggle-menu');
-	var mainNav = $('#main-nav');
-  toggleMenu.on('click', function() {
-    toggleMenu.add(mainNav).toggleClass('show');
-  });
-
-});
